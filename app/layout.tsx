ec2cf6ed1908488c533f08d669c02de6ab67e94c
Steve Hole <38703012+stevehole@users.noreply.github.com>
2025-07-29 12:50:32 -0400

    Fix: ensure layout.tsx includes html/body wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 // app/layout.tsx
-export const metadata = {
-  title: '1118 Site',
-  description: 'Website for 1118 Labs',
-};
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
